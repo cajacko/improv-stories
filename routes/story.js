297008cf246c8686692f3a0598a728be0075c316
Charlie Jackson <charlie@charliejackson.com>
2016-04-04 10:51:45 +0100

    Only show the back button when necessary

--- routes/story.js
+++ routes/story.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-   res.render('pages/story', {page: 'story', title: 'Story Title', userTurn: true});
+   res.render('pages/story', {page: 'story', title: 'Story Title', userTurn: true, back: '/'});
 });
 
 module.exports = router;
