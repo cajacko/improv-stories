b5227fda04e567088ede492b114b5fde99092fd7
Charlie Jackson <charlie@charliejackson.com>
2016-04-05 18:08:36 +0100

    Tidied up the main routes page

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,46 @@
-var express = require('express');
-var router = express.Router();
-var user = require('../models/user');
-var story = require('../models/story');
+/**
+ * Define all the routes and which scripts control them.
+ *
+ * Display the home page
+ */
 
-router.use('/add-story', require('./add_story'));
-router.use('/login/facebook', require('./login/facebook'));
-router.use('/next-story', require('./next_story'));
-router.use('/save-entry', require('./save_entry'));
-router.use('/story/*', require('./story'));
+var express = require('express'),
+    router = express.Router(),
+    user = require('../models/user'),
+    story = require('../models/story');
 
+router.use('/add-story', require('./add_story')); // Add story page and action
+router.use('/login/facebook', require('./login/facebook')); // Facebook login redirect
+router.use('/next-story', require('./next_story')); // Get the next story
+router.use('/save-entry', require('./save_entry')); // Save an entry
+router.use('/story/*', require('./story')); // Story page
+
+// Display the home page
 router.get('/', function(req, res, next) {
-  user.getUser(req, function(userDetails) {
-    if(userDetails) {
-      story.getUserStories(userDetails.id, function(err, stories) {
-        if(err) {
-          console.log('Error getting the user stories');
-          res.send('Error loading the dashboard');
-        } else if(stories.length) {
-          res.render('pages/dashboard', { stories: stories });
+    // Get the current user if they exists
+    user.getUser(req, function(userDetails) {
+        // If the user exists show the dashboard, otherwise show the login page
+        if(userDetails) {
+            // Get all the stories the current user is an author of
+            story.getUserStories(userDetails.id, function(err, stories) {
+                /** 
+                 * If there was an error then notify the user otherwise show the dashboard.
+                 *
+                 * If there are stories then show them
+                 */
+                if(err) {
+                    res.send('Error loading the dashboard');
+                } else if(stories.length) {
+                    res.render('pages/dashboard', { stories: stories });
+                } else {
+                    res.render('pages/dashboard', { stories: false });
+                }
+            });
         } else {
-          res.render('pages/dashboard', { stories: false });
+            // The user is not logged in, show the login page
+            res.render('pages/login');
         }
-      });
-    } else {
-      res.render('pages/login');
-    }
-  });
+    });
 });
 
 module.exports = router;
