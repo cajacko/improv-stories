2fd87ce44812f8327a6c1ec14b202676caddb6ff
Charlie Jackson <charlie@charliejackson.com>
2016-04-05 18:14:18 +0100

    Tidied up story route

--- routes/next_story.js
+++ routes/next_story.js
@@ -1,18 +1,24 @@
-var express = require('express');
-var router = express.Router();
-var story = require('../models/story');
+/**
+ * Get the next entry to a story
+ */
 
+var express = require('express'),
+    router = express.Router(),
+    story = require('../models/story');
+
+// Get all requests to '/next-story'
 router.get('/', function(req, res, next) {
+    // TODO: move this to the story page and get the id from there
 
+    // Get the last entry for the specified story
 	story.getLastEntry(15, function(err, entries) {
+        // If there was an error getting the next entry then return false, otherwise return the entry
 		if(err) {
 			res.json(false);
 		} else {
-			res.json(entries);
+			res.json(entries); // Return the entry
 		}
 	});
-
-	
 });
 
 module.exports = router;
