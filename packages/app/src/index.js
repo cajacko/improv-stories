fc2f5594a25b58111f4007e943922c1a6337a923
Charlie Jackson <charlie.jackson@eventstag.com>
2018-01-13 09:32:43 +0000

    Unique ids for each entry partial

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -3,47 +3,56 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 const storyEntries = [
   {
-    author: 'Bob',
-    finalEntryText: '.\nIn the toilet he did a piss',
+    id: '2',
+    author: 'Charlie',
+    entryPartials: [
+      {
+        id: 'a',
+        text:
+          'Once upon a time there was a turkey called Rob.\nOne day Rob went to the toilet',
+      },
+    ],
   },
   {
-    author: 'Charlie',
-    finalEntryText:
-      'Once upon a time there was a turkey called Rob.\nOne day Rob went to the toilet',
+    id: '1',
+    author: 'Bob',
+    entryPartials: [{ id: 'b', text: '.\nIn the toilet he did a piss' }],
   },
 ];
 
 const nextEntryPartials = [
   {
+    id: '3',
     author: 'Viki',
     entryPartials: [
-      ' ',
-      ' a',
-      ' an',
-      ' and',
-      ' and ',
-      ' and p',
-      ' and po',
-      ' and poo',
-      ' and pooe',
-      ' and pooed',
-      ' and pooed.',
+      { id: 'c', text: ' ' },
+      { id: 'd', text: ' a' },
+      { id: 'e', text: ' an' },
+      { id: 'f', text: ' and' },
+      { id: 'g', text: ' and ' },
+      { id: 'h', text: ' and p' },
+      { id: 'i', text: ' and po' },
+      { id: 'j', text: ' and poo' },
+      { id: 'k', text: ' and pooe' },
+      { id: 'l', text: ' and pooed' },
+      { id: 'm', text: ' and pooed.' },
     ],
   },
   {
+    id: '4',
     author: 'Viki',
     entryPartials: [
-      '\n',
-      '\nW',
-      '\nWh',
-      '\nWhe',
-      '\nWhen',
-      '\nWhen ',
-      '\nWhen h',
-      '\nWhen he',
-      '\nWhen h',
-      '\nWhen hi',
-      '\nWhen his',
+      { id: 'n', text: '\n' },
+      { id: 'o', text: '\nW' },
+      { id: 'p', text: '\nWh' },
+      { id: 'q', text: '\nWhe' },
+      { id: 'r', text: '\nWhen' },
+      { id: 's', text: '\nWhen ' },
+      { id: 't', text: '\nWhen h' },
+      { id: 'u', text: '\nWhen he' },
+      { id: 'v', text: '\nWhen h' },
+      { id: 'w', text: '\nWhen hi' },
+      { id: 'x', text: '\nWhen his' },
     ],
   },
 ];
@@ -51,20 +60,6 @@ const nextEntryPartials = [
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-
-    let storyText = '';
-
-    storyEntries.forEach(({ author, finalEntryText }) => {
-      storyText = `${finalEntryText}${storyText}`;
-    });
-
-    this.playing = false;
-
-    const storyParagraphs = storyText.split('\n');
-    this.prevStoryParagraphs = storyParagraphs;
-    this.nextEntryPartials = nextEntryPartials;
-
-    this.state = { storyParagraphs };
 
     this.play = this.play.bind(this);
     this.clearPlay = this.clearPlay.bind(this);
@@ -74,16 +69,39 @@ export default class App extends React.Component {
     this.addEntryTextToStoryParagraphs = this.addEntryTextToStoryParagraphs.bind(
       this,
     );
+    this.hasNextEntryPartials = this.hasNextEntryPartials.bind(this);
+
+    let storyParagraphs = [];
+
+    storyEntries.forEach(({ author, entryPartials }) => {
+      const { text, id } = entryPartials[entryPartials.length - 1];
+
+      storyParagraphs = this.addEntryTextToStoryParagraphs(
+        text,
+        storyParagraphs,
+        id,
+      );
+    });
+
+    this.prevStoryParagraphs = storyParagraphs;
+    this.nextEntryPartials = nextEntryPartials;
+
+    this.state = {
+      storyParagraphs,
+      hasNextEntryPartials: this.hasNextEntryPartials(),
+      playing: false,
+    };
   }
 
   clearPlay() {
-    this.playing = false;
+    this.setState({ playing: false });
     clearInterval(this.interval);
   }
 
   play() {
     if (this.nextEntryPartials.length) {
-      this.playing = true;
+      this.setState({ playing: true });
+
       this.interval = setInterval(() => {
         if (this.nextEntryPartials.length) {
           this.addNextEntryPartialToStoryParagraphs();
@@ -95,22 +113,18 @@ export default class App extends React.Component {
   }
 
   addNextEntryPartialToStoryParagraphs() {
-    if (
-      !this.nextEntryPartials.length ||
-      !this.nextEntryPartials[0].entryPartials.length
-    ) {
+    if (!this.state.hasNextEntryPartials) {
       this.clearPlay();
       return;
     }
 
-    const entryPartialToAdd = this.nextEntryPartials[0].entryPartials[0];
+    const { text, id } = this.nextEntryPartials[0].entryPartials[0];
 
     const nextStoryParagraphs = this.addEntryTextToStoryParagraphs(
-      entryPartialToAdd,
+      text,
       this.prevStoryParagraphs,
+      id,
     );
-
-    this.setState({ storyParagraphs: nextStoryParagraphs });
 
     if (this.nextEntryPartials[0].entryPartials.length === 1) {
       this.prevStoryParagraphs = nextStoryParagraphs;
@@ -120,39 +134,65 @@ export default class App extends React.Component {
         1,
       );
     }
+
+    this.setState({
+      storyParagraphs: nextStoryParagraphs,
+      hasNextEntryPartials: this.hasNextEntryPartials(),
+    });
   }
 
-  addEntryTextToStoryParagraphs(entryText, storyParagraphs) {
+  addEntryTextToStoryParagraphs(entryText, storyParagraphs, id) {
     let entryParagraphs = entryText.split('\n');
 
     if (!entryParagraphs.length) return storyParagraphs;
 
-    let nextStoryParagraphs = storyParagraphs.slice();
-    const lastStoryParagraphIndex = nextStoryParagraphs.length - 1;
-    const lastStoryParagraph = nextStoryParagraphs[lastStoryParagraphIndex];
+    entryParagraphs = entryParagraphs.map((text, i) => ({
+      text,
+      id: `${id}-text`,
+    }));
 
-    nextStoryParagraphs[lastStoryParagraphIndex] = `${lastStoryParagraph}${
-      entryParagraphs[0]
-    }`;
-    entryParagraphs = entryParagraphs.slice(1);
+    let nextStoryParagraphs = storyParagraphs.slice();
+
+    if (nextStoryParagraphs.length) {
+      const lastStoryParagraphIndex = nextStoryParagraphs.length - 1;
+      const lastStoryParagraph = nextStoryParagraphs[lastStoryParagraphIndex];
+      const firstEntryParagraph = entryParagraphs[0];
+
+      nextStoryParagraphs[lastStoryParagraphIndex] = {
+        text: `${lastStoryParagraph.text}${firstEntryParagraph.text}`,
+        id: `${lastStoryParagraph.id}-${firstEntryParagraph.id}`,
+      };
+
+      entryParagraphs = entryParagraphs.slice(1);
+    }
 
     nextStoryParagraphs = nextStoryParagraphs.concat(entryParagraphs);
     return nextStoryParagraphs;
+  }
+
+  hasNextEntryPartials() {
+    return (
+      this.nextEntryPartials.length &&
+      this.nextEntryPartials[0].entryPartials.length
+    );
   }
 
   render() {
     return (
       <View style={styles.container}>
         <View>
-          {this.state.storyParagraphs.map((text, i) => (
-            <Text key={`${i}-${text}`}>{text}</Text>
+          {this.state.storyParagraphs.map(({ text, id }) => (
+            <Text key={id}>{text}</Text>
           ))}
         </View>
-        <TouchableOpacity onPress={this.play}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Play</Text>
-          </View>
-        </TouchableOpacity>
+        {this.state.hasNextEntryPartials &&
+          !this.state.playing && (
+            <TouchableOpacity onPress={this.play}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Play</Text>
+              </View>
+            </TouchableOpacity>
+          )}
       </View>
     );
   }
