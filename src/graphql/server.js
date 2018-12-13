b3737645ffcb0a0d62196a2ccb3eb1c0dac14255
Charlie Jackson <charlie@charliejackson.com>
2018-12-13 12:49:27 +0000

    🛠 refactor(babel): Update to the new babel config
    - Original Branch: template-init

---
+++ src/graphql/server.js
@@ -0,0 +1,12 @@
+// @flow
+
+// import * as postResolvers from './post/resolvers';
+// import * as postTypeDefs from './post/typeDefs';
+
+export const resolvers = {
+  // postResolvers,
+};
+
+export const typeDefs = {
+  // postTypeDefs,
+};
