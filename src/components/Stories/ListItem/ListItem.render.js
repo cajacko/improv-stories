575d0f533ae3a7ca16e7cc78fbceebdeacfc3d21
Charlie Jackson <charlie@charliejackson.com>
2018-12-24 09:49:14 +0000

    ✨ feat(stories): Let you go to a specific story from the list view
    - Original Branch: multiple-stories

--- src/components/Stories/ListItem/ListItem.render.js
+++ src/components/Stories/ListItem/ListItem.render.js
@@ -3,18 +3,20 @@
 import React from 'react';
 import CardsListItem from '@cajacko/lib/components/Cards/ListItem';
 import Text from '@cajacko/lib/components/Cards/ListItem/Text';
+import withRouter from '@cajacko/lib/components/HOCs/withRouter';
 
 type Props = {
   title: string,
+  storyID: string,
 };
 
 /**
  * Displays an individual story title for the stories list
  */
-const ListItem = ({ title }: Props) => (
-  <CardsListItem horizontalPadding>
+const ListItem = ({ storyID, title, history: { push } }: Props) => (
+  <CardsListItem horizontalPadding action={() => push(`/story/${storyID}`)}>
     <Text text={{ _textFromConst: title }} />
   </CardsListItem>
 );
 
-export default ListItem;
+export default withRouter(ListItem);
