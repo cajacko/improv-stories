14022b2d14dcc40866363e02e856c1620c0f2937
Charlie Jackson <charlie@charliejackson.com>
2020-05-03 19:55:26 +0100

    ci(cd): 🔧 write .env.production files during deployment
    branch master

---
+++ .scripts/createEnv.js
@@ -0,0 +1,33 @@
+const { readFile, writeFile } = require("fs-extra");
+const { join } = require("path");
+
+const dirs = [
+  join(__dirname, "../packages/web-app"),
+  join(__dirname, "../packages/api"),
+];
+
+Promise.all(
+  dirs.map((dir) =>
+    readFile(join(dir, ".env")).then((content) => {
+      const string = content.toString();
+
+      const env = string
+        .split("\n")
+        .map((line) => line.trim())
+        .filter((line) => line !== "" || line.startsWith("#"))
+        .reduce((acc, line) => {
+          const [key] = line.split("=");
+
+          const value = process.env[key];
+
+          if (!value) {
+            throw new Error(`No env value for ${key}`);
+          }
+
+          return `${acc}\n${key}=${value}`;
+        }, "");
+
+      return writeFile(join(dir, ".env.production"), env);
+    })
+  )
+);
